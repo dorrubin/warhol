6f016b6fcce25bfbff3fc9a45cd59f1dbb398aba
Dor Rubin <dor.rubin@gmail.com>
2015-06-04 09:28:34 -0400

    added signup and signin

--- app.rb
+++ app.rb
@@ -4,10 +4,62 @@ module App
 			register Sinatra::Reloader
       set :sessions, true
 		end
+    
+    def current_user
+      session[:user_id]
+    end
 
+    def logged_in?
+      !current_user.nil?
+    end
+
+    # -- HOMEPAGE --
 		get ('/') do
 			erb :index
 		end
+
+		post '/signin' do
+			query = "SELECT * FROM users WHERE username = $1 AND password = $2"
+     	@user = $db.exec_params(query, [params[:username], params[:password]])
+	    if @user.first.nil?
+	    	#if password is wrong, redirect to homepage
+	    	@message = "Invalid Login"
+	      erb :index
+	     else
+	       user_id = @user.first["id"]
+	       #save user_id into the session
+	       session[:user_id] = user_id
+	       #redirect somewhere
+	       redirect "/"
+	    end
+    end
+    
+    # -- Collections --
+		get ('/collections') do
+			erb :collections
+		end
+
+    # -- REGISTER --
+		get ('/register') do
+			erb :register
+		end
+
+		post ('/register') do
+      name = params[:name]
+      email = params[:email]
+      username = params[:username]
+      phone = params[:phone]
+      password = params[:password]
+      query = "INSERT INTO users (name, email, username, phone, password) VALUES ($1, $2, $3, $4, $5) RETURNING id"
+      $db.exec_params(query, [name, email, username, phone, password])
+      redirect ("/browse")
+		end
+
+    # -- APP --
+		get ('/browse') do
+			 erb :browse, :layout => :app
+		end
+
 
 	end #class
 end #module
--- db/seeds.sql
+++ db/seeds.sql
@@ -5,5 +5,6 @@
 TRUNCATE TABLE users CASCADE;
 TRUNCATE TABLE cards CASCADE;
 
-INSERT INTO users (id, username, email, name, phone, password) VALUES (1, 'awarhol', 'andy@example.com', 'Andy Warhol', '555-555-5555', 'jk');
+INSERT INTO users (id, username, email, name, phone, password) VALUES (1, 'andy', 'andy@example.com', 'Andy Warhol', '555-555-5555', 'jk');
+INSERT INTO users (id, username, email, name, phone, password) VALUES (2, 'randy', 'randy@example.com', 'Randy Friend', '555-555-5555', 'jk');
 INSERT INTO cards (id, value, author, tags) VALUES (1, 'This is a test', 1, '{cats, dogs, soup}');
